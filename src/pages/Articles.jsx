f8e238e1882bc0f30346b6a88eaed5ac81e8310c
pierrehugendubel <pierrehugendubel@aol.com>
2026-03-01 22:49:02 +0000

    File changes

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -383,6 +383,7 @@ export default function Articles() {
                     selectedArticles={selectedArticles}
                     onClearSelection={clearSelection}
                 />
+                <AlertDialogComponent />
             </div>
         </div>
     );

ffac3e9abfa999872e62903a4948100680615655
pierrehugendubel <pierrehugendubel@aol.com>
2026-04-22 22:40:21 +0000

    File changes

--- src/components/articles/ArticleModal.jsx
+++ src/components/articles/ArticleModal.jsx
@@ -13,7 +13,6 @@ import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/u
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import BarcodeScanner from '@/components/restock/BarcodeScanner';
 import ImageEditor from '@/components/articles/ImageEditor';
-import AllergenEditor from '@/components/articles/AllergenEditor';
 import AllergenSelector from '@/components/menu/AllergenSelector';
 import { haptics } from "@/components/utils/haptics";
 import { toast } from 'sonner';
@@ -433,11 +432,6 @@ Berücksichtige typische Allergene: Gluten, Krebstiere, Eier, Fisch, Erdnüsse, 
                         )}
                     </div>
 
-                    <AllergenEditor
-                        value={formData.allergens}
-                        onChange={(val) => setFormData({ ...formData, allergens: val })}
-                        articleName={formData.name}
-                    />
                     <AllergenSelector
                         allergensList={formData.allergens_list}
                         additives={formData.additives}
